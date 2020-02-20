f8ac825af9796c332770ca347576d8d506cca7a7
premti <premti@gmail.com>
2020-02-20 11:11:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  length = array.length
+  length.times do |value_to_find|
+    return value_to_find
 end
-
+end
 def find_max_value(array)
   # Add your solution here
 end
