34e707cc448e817fda498abb58e390e74afa53ba
premti <premti@gmail.com>
2020-02-20 12:06:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,10 +4,11 @@
 
 def find_element_index(array, value_to_find)
   array.length.times do |count|
-    array[count] == value_to_find
+   if array[count] == value_to_find
     return count
   end
-  return nil
+  end
+  nil
 end
   
 def find_max_value(array)
