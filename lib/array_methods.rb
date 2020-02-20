7c6cfe2bef9fb16d82497cddf75b62511f67d716
premti <premti@gmail.com>
2020-02-20 12:07:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@
 #end
 
 def find_element_index(array, value_to_find)
-  array.length.times do |count|
-   if array[count] == value_to_find
-    return count
+  array.length.times do |index|
+   if array[index] == value_to_find
+    return index
   end
   end
   nil
